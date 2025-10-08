f0a3754cb56d3f48cd799d9e1c38cd9856e097c9
Francesco Trapani <franky_92@live.it>
2025-10-08 21:23:14 +0200

    Upp6

--- demo-REST-user-service/src/main/java/com/example/demo_REST_user_service/controller/UserController.java
+++ demo-REST-user-service/src/main/java/com/example/demo_REST_user_service/controller/UserController.java
@@ -1,4 +1,57 @@
 package com.example.demo_REST_user_service.controller;
 
+import com.example.demo_REST_user_service.model.dto.User;
+import com.example.demo_REST_user_service.service.IUserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/users")
 public class UserController {
+
+    private final IUserService userService;
+
+    @Autowired
+    public UserController(IUserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping // ?orderBy?name&direction=ASC
+    public List<User> getAllUsers() {
+        // TODO orderBy email, name, surname sia ASC che DESC
+        return userService.getAllUsers();
+    }
+
+    @GetMapping("/{email}")
+    public ResponseEntity<User> getUserByEmail(@PathVariable String email) {
+        User user = userService.getUserByEmail(email);
+        if (user == null) return ResponseEntity.notFound().build();
+        return ResponseEntity.ok(user);
+    }
+
+    @PostMapping
+    public ResponseEntity<User> createUser(@RequestBody User user) {
+        User createUser = userService.createUser(user);
+        return ResponseEntity.status(HttpStatus.CREATED).body(createUser);
+    }
+
+    @PutMapping("/{email}")
+    public ResponseEntity<User> updateUser(
+            @PathVariable String email,
+            @RequestBody User updateUser) {
+        User user = userService.updateUser(email, updateUser);
+        if (user == null) return ResponseEntity.notFound().build();
+        return ResponseEntity.ok(user);
+    }
+
+    @DeleteMapping("/{email}")
+    public ResponseEntity<Void> deleteUser(@PathVariable String email) {
+        boolean deleted = userService.deleteUser(email);
+        if (deleted) return ResponseEntity.noContent().build();
+        return ResponseEntity.notFound().build();
+    }
 }
