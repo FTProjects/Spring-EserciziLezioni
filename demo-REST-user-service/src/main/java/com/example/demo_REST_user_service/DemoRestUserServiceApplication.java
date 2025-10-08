9e8453becea26fee49d9c61bb65a4c4282d16595
Francesco Trapani <franky_92@live.it>
2025-10-08 20:13:05 +0200

    Nuovo Progetto

---
+++ demo-REST-user-service/src/main/java/com/example/demo_REST_user_service/DemoRestUserServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo_REST_user_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoRestUserServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoRestUserServiceApplication.class, args);
+	}
+
+}
---
+++ demo-REST-user-service/src/test/java/com/example/demo_REST_user_service/DemoRestUserServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demo_REST_user_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoRestUserServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
