a1f57a56fc6acbdb10d57de9ef550d5ef2ba1059
Francesco Trapani <franky_92@live.it>
2025-10-08 20:34:59 +0200

    Upp2

--- demo-REST-user-service/src/main/java/com/example/demo_REST_user_service/model/dto/User.java
+++ demo-REST-user-service/src/main/java/com/example/demo_REST_user_service/model/dto/User.java
@@ -1,4 +1,31 @@
 package com.example.demo_REST_user_service.model.dto;
 
-public record User() {
+// Un record è una data class di oggetti immutabili
+
+/**
+ * Un record è una data class di oggetti immutabili
+ *
+ * @param name    Il nome dell'utente.
+ * @param surname Il cognome dell'utente.
+ * @param email   L'email dell'utente.
+ */
+public record User( // il costruttore del record User
+                    String name,    // la proprietà del record user
+                    String surname, // la proprietà del record user
+                    String email    // la proprietà del record user
+) {
+
+    /**
+     * Validatore del Costruttore per il record User
+     * Valida che i campi non siano nulli o vuoti
+     * @throws IllegalArgumentException se uno dei campi è nullo o vuoto
+     */
+    public User {
+        if (name == null || name.isBlank())
+            throw new IllegalArgumentException("Name cannot be null!");
+        if (surname == null || surname.isBlank())
+            throw new IllegalArgumentException("Surname cannot be null!");
+        if (email == null || email.isBlank())
+            throw new IllegalArgumentException("Email cannot be null!");
+    }
 }
--- demo-REST-user-service/src/main/java/com/example/demo_REST_user_service/service/IUserService.java
+++ demo-REST-user-service/src/main/java/com/example/demo_REST_user_service/service/IUserService.java
@@ -1,4 +1,56 @@
 package com.example.demo_REST_user_service.service;
 
+import com.example.demo_REST_user_service.model.dto.User;
+
+import java.util.List;
+
+/**
+ * Interfaccia per la gestione delle operazioni CRUD sugli Utenti
+ */
 public interface IUserService {
+
+    // ***************** Read   -> get
+
+    /**
+     * Restituisce tutti gli utenti del sistema
+     * @return Una lista di Users
+     */
+    List<User> getAllUsers();
+
+    /**
+     * Restituisce un utente dato il suo indirizzo email
+     * @param email L'email dell'utente
+     * @return L'utente corrispondente o null se non esiste
+     */
+    User getUserByEmail(String email);
+
+    // ****************** Create -> post
+
+    /**
+     * Crea un nuovo utente
+     * @param user L'utente da creare
+     * @return L'utente creato o null se esiste già
+     */
+    User createUser(User user);
+
+
+    // ****************** Update -> put/patch
+
+    /**
+     * Aggiorna utente esistente.
+     * @param email L'email dell'utente da aggiornare
+     * @param updateUser I dati aggiornati dell'utente
+     * @return L'utente aggiornato o null se non esiste
+     */
+    User updateUser(String email, User updateUser);
+
+    // ****************** Delete -> delete
+
+    /**
+     * Elimina un utente dato il suo indirizzo email
+     * @param email L'email dell'utente da eliminare
+     * @return TRUE se l'utente viene eliminato, FALSE altrimenti
+     */
+    boolean deleteUser(String email);
+
 }
