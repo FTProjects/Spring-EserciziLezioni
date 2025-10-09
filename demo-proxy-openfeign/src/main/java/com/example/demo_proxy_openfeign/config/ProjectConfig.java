b489c3883ce9a1f49f0d0b3797ef1ffefc1ec33d
Francesco Trapani <franky_92@live.it>
2025-10-09 19:36:00 +0200

    Aggiungi configurazione per OpenFeign e implementa PaymentsController con gestione delle richieste di pagamento

--- demo-proxy-openfeign/src/main/java/com/example/demo_proxy_openfeign/config/ProjectConfig.java
+++ demo-proxy-openfeign/src/main/java/com/example/demo_proxy_openfeign/config/ProjectConfig.java
@@ -1,4 +1,10 @@
 package com.example.demo_proxy_openfeign.config;
 
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+// abilita il client openfeign scansionando il package dove è incluso il nostro proxy
+@EnableFeignClients(basePackages = "com.example.demo_proxy_openfeign.proxy")
 public class ProjectConfig {
 }
--- demo-proxy-openfeign/src/main/java/com/example/demo_proxy_openfeign/controller/PaymentsController.java
+++ demo-proxy-openfeign/src/main/java/com/example/demo_proxy_openfeign/controller/PaymentsController.java
@@ -1,4 +1,29 @@
 package com.example.demo_proxy_openfeign.controller;
 
+import com.example.demo_proxy_openfeign.model.Payment;
+import com.example.demo_proxy_openfeign.proxy.PaymentProxy;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.UUID;
+
+@RestController
 public class PaymentsController {
+
+    private final PaymentProxy proxy;
+
+    @Autowired
+    public PaymentsController(PaymentProxy proxy) {
+        this.proxy = proxy;
+    }
+
+    @PostMapping("/pagamenti")
+    public Payment createPayment(@RequestBody Payment payment) {
+        // genero un requestId da inserire in un header
+        String requestId = UUID.randomUUID().toString();
+        // delego la chiamata al proxy
+        return proxy.createPayment(requestId, payment);
+    }
 }
