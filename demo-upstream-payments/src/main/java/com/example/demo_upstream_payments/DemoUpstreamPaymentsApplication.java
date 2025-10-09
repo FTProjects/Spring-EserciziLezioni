768feca47ccfc6a05ff8f425f488e304e8b6fd5e
Francesco Trapani <franky_92@live.it>
2025-10-09 18:36:51 +0200

    Aggiungi file di configurazione Maven e struttura di progetto Spring Boot

---
+++ demo-upstream-payments/src/main/java/com/example/demo_upstream_payments/DemoUpstreamPaymentsApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo_upstream_payments;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoUpstreamPaymentsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoUpstreamPaymentsApplication.class, args);
+	}
+
+}
---
+++ demo-upstream-payments/src/test/java/com/example/demo_upstream_payments/DemoUpstreamPaymentsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demo_upstream_payments;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoUpstreamPaymentsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
